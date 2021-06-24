ad16c74e3ac00eb0643112831b99a4b435e275f0
Frederik Mahler-Andersen <hello@fm-a.dk>
2021-06-24 12:51:48 +0000

    1.1.7 – kirby, composer, npm updates

--- kirby/src/Cache/FileCache.php
+++ kirby/src/Cache/FileCache.php
@@ -2,8 +2,10 @@
 
 namespace Kirby\Cache;
 
+use Kirby\Exception\Exception;
 use Kirby\Toolkit\Dir;
 use Kirby\Toolkit\F;
+use Kirby\Toolkit\Str;
 
 /**
  * File System Cache Driver
@@ -68,7 +70,41 @@ class FileCache extends Cache
      */
     protected function file(string $key): string
     {
-        $file = $this->root . '/' . basename($key);
+        // strip out invalid characters in each path segment
+        // split by slash or backslash
+        $keyParts = [];
+        foreach (preg_split('#([\/\\\\])#', $key, 0, PREG_SPLIT_DELIM_CAPTURE) as $part) {
+            switch ($part) {
+                // forward slashes don't need special treatment
+                case '/':
+                    break;
+
+                // backslashes get their own marker in the path
+                // to differentiate the cache key from one with forward slashes
+                case '\\':
+                    $keyParts[] = '_backslash';
+                    break;
+
+                // empty part means two slashes in a row;
+                // special marker like for backslashes
+                case '':
+                    $keyParts[] = '_empty';
+                    break;
+
+                // an actual path segment
+                default:
+                    // check if the segment only contains safe characters;
+                    // underscores are *not* safe to guarantee uniqueness
+                    // as they are used in the special cases
+                    if (preg_match('/^[a-zA-Z0-9-]+$/', $part) === 1) {
+                        $keyParts[] = $part;
+                    } else {
+                        $keyParts[] = Str::slug($part) . '_' . sha1($part);
+                    }
+            }
+        }
+
+        $file = $this->root . '/' . implode('/', $keyParts);
 
         if (isset($this->options['extension'])) {
             return $file . '.' . $this->options['extension'];
@@ -143,10 +179,41 @@ class FileCache extends Cache
     {
         $file = $this->file($key);
 
-        if (is_file($file) === true) {
-            return F::remove($file);
-        } else {
-            return false;
+        if (is_file($file) === true && F::remove($file) === true) {
+            $this->removeEmptyDirectories(dirname($file));
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Removes empty directories safely by checking each directory
+     * up to the root directory
+     *
+     * @param string $dir
+     * @return void
+     */
+    protected function removeEmptyDirectories(string $dir): void
+    {
+        try {
+            // ensure the path doesn't end with a slash for the next comparison
+            $dir = rtrim($dir, '/\/');
+
+            // checks all directory segments until reaching the root directory
+            while (Str::startsWith($dir, $this->root()) === true && $dir !== $this->root()) {
+                $files = array_diff(scandir($dir) ?? [], ['.', '..']);
+
+                if (empty($files) === true && Dir::remove($dir) === true) {
+                    // continue with the next level up
+                    $dir = dirname($dir);
+                } else {
+                    // no need to continue with the next level up as `$dir` was not deleted
+                    break;
+                }
+            }
+        } catch (Exception $e) { // @codeCoverageIgnore
+            // silently stops the process
         }
     }
 
